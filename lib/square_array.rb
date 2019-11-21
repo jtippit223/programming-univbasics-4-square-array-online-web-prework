7cad3cb462250e43fda83c5f9bc62b21c0cae524
Jesse Tippit <jtippit223@gmail.com>
2019-11-21 06:27:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,7 @@
-def square_array()
+def square_array(numbers)
   count = 0
   while count < Array.length do
     numbers[count] ** 2
     count += 1
     end
 end
-numbers = [1,2,3]
-square_array(numbers)
-new_numbers = [9,10,16,25]
-square_array(new_numbers)
