0d341463876b4afffc5d04ed1bed00acc0303cdd
Jesse Tippit <jtippit223@gmail.com>
2019-11-21 06:34:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-    index = array.length -1
-  
-    array[index] = array[index] * array[index]
-    index = index - 1
+  counter = 0
+  while counter < array.length do
+    array[counter] = array[counter] * array[counter]
+    counter += 1
   end
   array
 end
